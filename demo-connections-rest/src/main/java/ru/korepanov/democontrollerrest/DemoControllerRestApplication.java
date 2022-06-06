6bb72cc614da9b17c8594c8fc9d692cf38822250
Корепанов Евгений Сергеевич <es.korepanov@gmail.com>
2022-06-06 20:37:53 +0400

    init commit

---
+++ demo-connections-rest/src/main/java/ru/korepanov/democontrollerrest/DemoControllerRestApplication.java
@@ -0,0 +1,13 @@
+package ru.korepanov.democontrollerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoControllerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoControllerRestApplication.class, args);
+    }
+
+}
